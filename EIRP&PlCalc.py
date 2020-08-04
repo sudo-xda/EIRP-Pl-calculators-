bda59becc3b71c5ac57051488621a89b72d6c663
Jigyansha Jeevan Rout <62542350+sudo-xda@users.noreply.github.com>
2020-08-04 10:06:29 +0530

    Create EIRP&PlCalc.py

---
+++ EIRP&PlCalc.py
@@ -0,0 +1,28 @@
+import math
+print("EIRP & Pl calculator by sudo_xda")
+
+Pt = float(input("Enter value of Pt in W/dBd:"))
+Gt = float(input("Enter value of Gt in dBd:"))
+Gr = float(input("Enter value of Gr in dBd:"))
+Pr = float(input("Enter value of Pr in dBm:"))
+Lt = float(input("Enter value of Lt in dB:"))
+Lr = float(input("Enter value of Lr in dB:"))
+
+Gt = (Gt+2.15 )
+Gr = (Gr+2.15)
+Pr = (Pr-31)
+
+Eirp = float(Pt+Gt-Lt)
+print('EIRP in dBw:')
+print(Eirp)
+print("EIRP in watt:")
+s = (Eirp/10.0)
+print(10**s)
+
+Pl = float(Pt+Gt+Gr-Pr-Lt-Lr)
+print("PL in dB:")
+print(Pl)
+
+
+
+
